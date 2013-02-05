0d5ef1b2b9bec555410524eb410d66d9daa8546f
BBK-PiJ-2012-06 <simon.ward42@gmail.com>
2013-02-05 15:41:50 +0000

    created Airplane class and constructor

--- MySecondProject/AirplaneTest.java
+++ MySecondProject/AirplaneTest.java
@@ -2,11 +2,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import vehicles.Airplane;
+
 
 public class AirplaneTest {
 
 	@Test
 	public void test() {
+		
+		Airplane classUnderTest = new Airplane(1);
+		
 		fail("Not yet implemented");
 	}
 
---
+++ MySecondProject/src/vehicles/Airplane.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package vehicles;
+
+/**
+ * @author sward09
+ *
+ */
+public class Airplane {
+	
+	private int kind;
+
+	public Airplane(int i) {
+		kind = i;
+	}
+
+}
